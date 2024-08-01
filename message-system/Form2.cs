ce27c176142970587396b66d42ba72417bef5ea6
aliwert <154356044+aliwert@users.noreply.github.com>
2024-08-01 12:42:30 +0300

    feat: inbox

--- message-system/Form2.cs
+++ message-system/Form2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,9 +19,26 @@ namespace message_system
         }
 
         public string number;
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-LRMEISB\SQLEXPRESS;Initial Catalog=Test;Integrated Security=True");
+
+        void inbox()
+        {
+            SqlDataAdapter da1 = new SqlDataAdapter("Select * From Tblmessages WHERE RECEIVER=" + number, con);
+            DataTable dt1 = new DataTable();
+            da1.Fill(dt1);
+            dataGridView1.DataSource = dt1;
+        }
+        void sent()
+        {
+            SqlDataAdapter da2 = new SqlDataAdapter("Select * From Tblmessages WHERE RECEIVER=" + number, con);
+            DataTable dt2 = new DataTable();
+            da2.Fill(dt2);
+            dataGridView1.DataSource = dt2;
+        }
         private void Form2_Load(object sender, EventArgs e)
         {
             lblNumber.Text = number;
+            inbox();
         }
     }
 }
